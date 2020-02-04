b438791f492f65f8e2f11ed99a20b3c20b6f4d55
Sergii Mostepan <sergiimostepan@gmail.com>
2020-02-04 21:57:18 +0200

    made hw 11

--- hw_11/task_03/timer.js
+++ hw_11/task_03/timer.js
@@ -8,5 +8,11 @@ const timer = new Timer({
     targetDate: new Date('Jul 17, 2020'),
 })
 
+const timer2 = new Timer({
+    selector: '#timer-2',
+    targetDate: new Date('Feb 10, 2020'),
+})
+
 
 timer.start();
+timer2.start();
--- hw_11/task_03/timerPlug.js
+++ hw_11/task_03/timerPlug.js
@@ -1,39 +1,39 @@
 export default class Timer {
-    constructor({
-        selector,
-        targetDate
-    }) {
-        this.selector = selector;
-        this.targetDate = targetDate;
-        this.days = document.querySelector('span[data-value="days"]');
-        this.hours = document.querySelector('span[data-value="hours"]');
-        this.mins = document.querySelector('span[data-value="mins"]');
-        this.secs = document.querySelector('span[data-value="secs"]');
-    };
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
+    this.days = this.selector.querySelector('span[data-value="days"]');
+    this.hours = this.selector.querySelector('span[data-value="hours"]');
+    this.mins = this.selector.querySelector('span[data-value="mins"]');
+    this.secs = this.selector.querySelector('span[data-value="secs"]');
+  }
 
-    start() {
-        this.timerId = setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = this.targetDate - currentTime;
-            const time = new Date(deltaTime);
-            const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-            const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-            const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-            const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-            this.updateClockFase(days, hours, mins, secs)
-            // console.log(`days: ${days} hours: ${hours} mins: ${mins} secs: ${secs}`)
-        }, 1000);
-    }
+  start() {
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = new Date(deltaTime);
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = this.pad(
+        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      this.updateClockFase(days, hours, mins, secs);
+      // console.log(`days: ${days} hours: ${hours} mins: ${mins} secs: ${secs}`)
+    }, 1000);
+  }
 
-    pad(value) {
-        return String(value).padStart(2, '0');
-    }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 
-    updateClockFase(days, hours, mins, secs) {
-        this.days.textContent = days;
-        this.hours.textContent = hours;
-        this.mins.textContent = mins;
-        this.secs.textContent = secs;
-    }
-
+  updateClockFase(days, hours, mins, secs) {
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
 }
