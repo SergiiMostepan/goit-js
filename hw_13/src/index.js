c9e50fe014b056fce33c79cd6b14cafaea3aaf42
Sergii Mostepan <sergiimostepan@gmail.com>
2020-02-23 14:27:38 +0200

    done hw

--- hw_13/src/index.js
+++ hw_13/src/index.js
@@ -4,3 +4,5 @@ import './js_components/main.js';
 import 'pnotify/dist/PNotifyBrightTheme.css';
 import 'basiclightbox/dist/basiclightbox.min.css';
 import './css/loader.css'
+import './css/toTop.css'
+import './js_components/toTOp.js'
